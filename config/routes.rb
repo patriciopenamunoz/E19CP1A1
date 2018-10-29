72ec4d3d5e6b9b30e7b4200ad4b4d796569422e3
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-10-29 16:56:03 -0300

    Add rest routes structure for peoples

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   resources :projects
+  resources :peoples
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
