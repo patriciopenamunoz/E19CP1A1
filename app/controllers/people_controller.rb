4ac54735b7b4304b00eb3d15b99f2a79596d7125
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-10-29 17:33:16 -0300

    Allow add and show new people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,2 +1,24 @@
 class PeopleController < ApplicationController
+  def new
+    @person = Person.new
+  end
+
+  def create
+    @person = Person.new(person_params)
+    if @person.save
+      redirect_to person_path(@person.id), notice: 'Person added.'
+    else
+      redirect_to new_person_path, alert: 'Error detected: Person not added.'
+    end
+  end
+
+  def show
+    @person = Person.find(params[:id])
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:first_name, :last_name)
+  end
 end
