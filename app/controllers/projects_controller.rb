08a826bd4c30e901e786c5da7038896006a512a0
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-10-29 16:50:42 -0300

    Allow add and show new projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,27 @@ class ProjectsController < ApplicationController
 
   def index
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to project_path(@project.id), notice: 'Project added.'
+    else
+      redirect_to new_project_path, alert: 'Error detected: Project not added.'
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
 end
