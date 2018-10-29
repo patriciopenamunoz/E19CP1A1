621c6ccb775e9e5d58a509df1119b1a95eec072f
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-10-29 17:26:09 -0300

    Rename people to person (use people only as plural)

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     sessions: 'users/sessions'
   }
   resources :projects
-  resources :peoples
+  resources :people
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class PeoplesControllerTest < ActionDispatch::IntegrationTest
+class PeopleControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
